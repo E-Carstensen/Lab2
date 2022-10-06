9079b34f398d04734fd4538706a85947593ca09c
Eric <ecarstensen99@gmail.com>
2022-10-05 19:10:37 -0600

    Final

---
+++ Eric Carstensen 3070801 CMPT361 AS01/client.py
@@ -0,0 +1,139 @@
+"""
+Eric Carstensen - 3070801
+CMPT 361 - AS01
+Lab 2
+"""
+
+import socket
+import sys
+
+#Main client loop, calls menu to take user choice in operation then calls
+#matching subroutine
+def main():
+    #initiate server connection
+    clientSocket = connect()
+    try:
+        while (1):
+            #Call menu funtion to get user choice and send to server
+            option = menu()
+            clientSocket.send(option.encode('ascii'))
+
+            #run corresponding subroutine
+            if (option == '1'):
+                add_contact(clientSocket)
+            elif(option == '2'):
+                search(clientSocket)
+            elif(option == '3'):
+                disconnect(clientSocket)
+                break
+
+    #Catch errors in clientSocket creation and handling
+    except socket.error as e:
+        print("Error:", e)
+        clientSocket.close()
+        sys.exit(1)
+
+    #If loop is broken, close connection and exit
+    clientSocket.close()
+    sys.exit(1)
+
+
+
+#Displays main menu options, takes input and returns string if input is valid
+def menu():
+
+    #Print menu options and take user input
+    option = input("""Please select the operation
+1)Add a new entry
+2)Search
+3)Terminate the connection
+
+Choice: """)
+
+    #If input is one of options and only 1 character
+    if(option in "123" and len(option) == 1):
+        return option
+    else:
+        #The input is not one of the options or is more than 1 character
+        print("Input Not Recognized")
+        #Recursively call menu function
+        return menu()
+
+
+
+
+#Take server address from user, initiate socket, and connect to server
+#Returns clientSocket object
+def connect():
+    #Default server information
+    serverName = '127.0.0.1'
+    serverPort = 13000
+
+    #Take server name from user
+    temp = input("Enter the server name or IP address: ")
+    if (len(temp) != 0):
+        #If user does not enter an address, use default serverName
+        serverName = temp
+
+    #Attempt to create client socket with IPv4 and TCP protocols
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Error in client socket creation:',e)
+        sys.exit(1)
+
+    #Attempt to connect clientSocket to given server name and port 13000
+    try:
+        clientSocket.connect((serverName, serverPort))
+        print("Welcome to the online phone book. \n")
+    except socket.error as e:
+        print("Error:", e)
+        clientSocket.close()
+        sys.exit(1)
+
+    #return initiated clientSocket object
+    return clientSocket
+
+
+#Search subroutine, sends search term then recieves and prints formatted result
+def search(clientSocket):
+    #recieve prompt from server
+    message = clientSocket.recv(2048).decode('ascii')
+    #take search term as input from user
+    val = input(message)
+    #send server search term
+    clientSocket.send(val.encode('ascii'))
+    #recieve formatted string of all contacts with matching info
+    result = clientSocket.recv(2048).decode('ascii')
+    #print result to user
+    print(result)
+
+#Add contact subroutine, takes prompts from server and sends new contact info
+def add_contact(clientSocket):
+    #recieve "Enter Name" prompt
+    message = clientSocket.recv(2048).decode('ascii')
+    #take new contact name info from user
+    name = input(message)
+    #send name of new contact to server
+    clientSocket.send(name.encode('ascii'))
+    #recieve "Enter Number" prompt
+    message = clientSocket.recv(2048).decode('ascii')
+    #take new number info from user
+    num = input(message)
+    #send number for new contact to server
+    clientSocket.send(num.encode('ascii'))
+
+
+#Terminate the connection with the server
+def disconnect(clientSocket):
+    #Send the server the disconnect operation
+    clientSocket.send('3'.encode('ascii'))
+    #Close the socket
+    clientSocket.close()
+    #Print message to user to inform the connection has been closed
+    print("Connection Terminated")
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ Eric Carstensen 3070801 CMPT361 AS01/server.py
@@ -0,0 +1,132 @@
+"""
+Eric Carstensen - 3070801
+CMPT 361 - AS01
+Lab 2
+"""
+
+import socket
+import sys
+
+def main():
+    contacts = {}
+
+    serverSocket = connect()
+    try:
+        while 1:
+            try:
+                connectionSocket, addr = serverSocket.accept()
+                #print(addr, " Has Connected to Socket: ", connectionSocket)
+
+                while 1:
+
+                    operation = connectionSocket.recv(2048).decode('ascii')
+
+                    if (operation == '1'):
+                        add_contact(connectionSocket, contacts)
+                    elif (operation == '2'):
+                        search(connectionSocket, contacts)
+                    elif (operation == '3'):
+                        #connectionSocket.close()
+                        break
+                    else:
+                        connectionSocket.send("Invalid Input".encode('ascii'))
+
+
+            except socket.error as e:
+                print('An error occured:',e)
+                connectionSocket.close()
+                serverSocket.close()
+                sys.exit(1)
+
+            connectionSocket.close()
+
+
+    except KeyboardInterrupt:
+            #connectionSocket.close()
+            serverSocket.close()
+            sys.exit(1)
+
+
+#Create and bind server socket, returns serverSocket object
+def connect():
+
+    #Server Port
+    serverPort = 13000
+
+    #Create socket using IPv4 and TCP protocols
+    try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print("Error Creating Socket: ", e)
+        sys.exit(1)
+
+    #Bind Server Socket to chosen port
+    try:
+        serverSocket.bind(('', serverPort))
+    except socket.error as e:
+        print("Error Binding Socket: ", e)
+        sys.exit(1)
+
+    #Set connection queue to max 1
+    serverSocket.listen(1)
+
+    #return serverSocket object
+    return serverSocket
+
+
+
+
+
+
+
+def add_contact(connectionSocket, contacts):
+
+    connectionSocket.send("Enter Name: ".encode('ascii'))
+    name = connectionSocket.recv(2048).decode('ascii')
+
+    connectionSocket.send("Enter Number: ".encode('ascii'))
+    num = connectionSocket.recv(2048).decode('ascii')
+
+    if (name in contacts):
+        contacts[name].append(num)
+    else:
+        contacts[name] = [num]
+
+    return contacts
+
+#Sends prompt and recieves search term
+#Returns and sends formatted search result string
+def search(connectionSocket, contacts):
+    #Define header text
+    result = "\n\nName:\t     PhoneNumber(s):\n"
+
+    #send client prompt message
+    connectionSocket.send("Enter the Search Word: ".encode('ascii'))
+    #recieve search term
+    value = connectionSocket.recv(2048).decode('ascii')
+
+    #For contact in contact dictionary
+    for key, nums in contacts.items():
+        #If the search term is in the key
+        if (value in key):
+            #append formatted information
+            result += (f"{key:<13}{nums!s}\n")
+            continue
+        #For each phone number for this contact
+        for num in nums:
+            #If the search term is in the phone number
+            if value in num:
+                #append entire contact, formatted
+                result += (f"{key:<13}{nums!s}\n")
+
+                continue
+    #Send results to client and return
+    connectionSocket.send(result.encode('ascii'))
+    return result
+
+
+
+
+
+
+main()
